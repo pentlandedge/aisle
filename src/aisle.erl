4671e9c329af185640839f6a29ceccd9f67d8002
Robert Forbes <robert.forbes@pentlandedge.com>
2016-11-20 21:57:44 +0000

    Added decoding of the radio channel. Also added a function to tokenise a string without skipping over double separators denoting empty fields.

--- src/aisle.erl
+++ src/aisle.erl
@@ -26,33 +26,44 @@
     get_frag_num/1,
     get_msg_id/1,
     get_data/1,
-    get_fill_bits/1]).
+    get_fill_bits/1,
+    to_tokens/2]).
 
--record(ais, {id, frag_count, frag_num, msg_id, data, fill_bits}).
+-record(ais, {id, frag_count, frag_num, msg_id, radio_chan, data, fill_bits}).
 
 %% API
 
 %% @doc Decode an AIS sentence.
 decode(Sentence) when is_list(Sentence) ->
-    Tokens = string:tokens(Sentence, ",*"),
-    [Id, FragCount, FragNum, MsgID|_Rest] = Tokens,
+    Tokens = to_tokens(Sentence, ",*"),
+    [Id, FragCount, FragNum, MsgID, Chan|_Rest] = Tokens,
     case Id of 
         "!AIVDM" -> 
             AisRec = #ais{
                 id = aivdm,
                 frag_count = list_to_integer(FragCount),
                 frag_num = list_to_integer(FragNum),
-                msg_id = decode_msg_id(MsgID)},
+                msg_id = decode_msg_id(MsgID),
+                radio_chan = decode_radio_chan(Chan)},
             {ok, AisRec};
         _ -> 
             {error, bad_identifier}
     end.
 
+%% @doc Decode the message ID field. This is often not set, so we need to trap
+%% this.
 decode_msg_id(MsgID)  ->
     case string:to_integer(MsgID) of
         {error, _} -> undefined;
         {X, _}     -> X
     end.
+
+%% @doc Decode the radio channel, either 'A' at 161.975 MHz or 'B' at 
+%% 162.025 MHz. Sometimes mapped as '1' or '2'.
+decode_radio_chan("A") -> radio_chan_a;
+decode_radio_chan("B") -> radio_chan_b;
+decode_radio_chan("1") -> radio_chan_a;
+decode_radio_chan("2") -> radio_chan_b.
 
 %% Accessor functions for the AIS records.
 get_id(#ais{id = X}) -> X. 
@@ -61,4 +72,27 @@ get_frag_num(#ais{frag_num = X}) -> X.
 get_msg_id(#ais{msg_id = X}) -> X. 
 get_data(#ais{data = X}) -> X. 
 get_fill_bits(#ais{fill_bits = X}) -> X. 
+
+%% @doc Utility function to work like string:tokens/1, but not skip over 
+%% multiple occurrences of the separator.
+to_tokens(String, SepList) when is_list(String), is_list(SepList) ->
+    toks(String, SepList, [], last_sep).
+
+toks([], _, Toks, _) ->
+    lists:reverse(Toks);
+toks(String, SepList, Toks, Last) ->
+    SepPoint = string:cspan(String, SepList),
+    case SepPoint of
+        0 ->
+            case Last of
+                not_sep ->
+                    toks(string:sub_string(String, 2), SepList, Toks, last_sep);
+                last_sep ->
+                    toks(string:sub_string(String, 2), SepList, [""|Toks], last_sep)
+            end;
+        N -> 
+            Tok = string:sub_string(String, 1, N),
+            Rem = string:sub_string(String, N+1),
+            toks(Rem, SepList, [Tok|Toks], not_sep)
+    end.
 
