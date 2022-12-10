9d0554879fdea03bebc7afc11fdad4e5174bdd6d
Robert Forbes <robert.forbes@pentlandedge.com>
2022-12-10 00:10:44 +0000

    Added character decoding of the location.

--- src/weather_obs.erl
+++ src/weather_obs.erl
@@ -19,12 +19,16 @@
 
 %% @doc Decode the IMO289 weather observations. 
 -spec decode(binary()) -> {ok, weather_obs()} | {error, Reason::atom()}.
-decode(<<MT:6,RI:2,MMSI:30,DAC:10,FID:6,V:1,_Rem/bitstring>>) ->
+decode(<<MT:6,RI:2,MMSI:30,DAC:10,FID:6,V:1,Loc:120/bitstring,_Rem/bitstring>>) ->
     {ok, #weather_obs{
         message_type = aisle:decode_message_type(MT),
         repeat_indicator = aisle:decode_repeat_indicator(RI),
         mmsi = MMSI,
         dac = DAC,
         fid = FID,
-        variant = V 
+        variant = V,
+        location = decode_location(Loc)
     }}.
+
+decode_location(Bin) ->
+    [sixbit:decode(X) || <<X:6>> <= Bin].
