e53ad3636fe7fc8dcac8e26921d43df158d5ee57
Robert Forbes <robert.forbes@pentlandedge.com>
2022-12-11 22:19:47 +0000

    Added more weather observation decoding.

--- src/weather_obs.erl
+++ src/weather_obs.erl
@@ -2,7 +2,7 @@
 
 -export([decode/1]).
 
--record(weather_obs, {
+-record(weather_obs_non_wmo, {
     message_type,
     repeat_indicator,
     mmsi,
@@ -32,23 +32,28 @@
     swell_direction,
     swell_period}).
 
--opaque weather_obs() :: #weather_obs{}.
--export_type([weather_obs/0]).
+-opaque weather_obs_non_wmo() :: #weather_obs_non_wmo{}.
+-export_type([weather_obs_non_wmo/0]).
 
 %% @doc Decode the IMO289 weather observations. 
--spec decode(binary()) -> {ok, weather_obs()} | {error, Reason::atom()}.
-decode(<<MT:6,RI:2,MMSI:30,DAC:10,FID:6,V:1,Loc:120/bitstring,_Lon:25,_Lat:24,
-    _Day:5,_Hr:5,_Min:6,_Pres:4,_Vis:1,_HVis:7,_RelHum:7,_AveWS:7,_WindDir:9,
+-spec decode(binary()) -> {ok, weather_obs_non_wmo()} | {error, Reason::atom()}.
+decode(<<MT:6,RI:2,MMSI:30,DAC:10,FID:6,0:1,Loc:120/bitstring,_Lon:25,_Lat:24,
+    _Day:5,_Hr:5,Min:6,_Pres:4,_Vis:1,_HVis:7,_RelHum:7,_AveWS:7,_WindDir:9,
     _AirP:9,_Rem/bitstring>>) ->
-    {ok, #weather_obs{
+    {ok, #weather_obs_non_wmo{
         message_type = aisle:decode_message_type(MT),
         repeat_indicator = aisle:decode_repeat_indicator(RI),
         mmsi = MMSI,
         dac = DAC,
         fid = FID,
-        variant = V,
-        location = decode_location(Loc)
+        variant = 0,
+        location = decode_location(Loc),
+
+        utc_minute = decode_utc_minute(Min)
     }}.
 
 decode_location(Bin) ->
     [sixbit:decode(X) || <<X:6>> <= Bin].
+
+decode_utc_minute(60) -> not_available;
+decode_utc_minute(X) when X >= 0, X =< 59 -> X.
